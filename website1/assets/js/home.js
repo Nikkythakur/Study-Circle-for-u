229a673485dbd3758f09a96361fefd2cf582cbe4
Nikita Thakur <77138407+Nikkythakur@users.noreply.github.com>
2021-12-12 15:13:38 +0530

    Add files via upload

---
+++ website1/assets/js/home.js
@@ -0,0 +1,33 @@
+$('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+	
+$("#menu-icon").on("click", function(){
+$("nav").slideToggle();
+$(this).toggleClass("active");
+});
+
+//** FAQ ** //
+(function($) {
+	'use strict';
+	
+	jQuery(document).on('ready', function(){
+	
+			$('a.page-scroll').on('click', function(e){
+				var anchor = $(this);
+				$('html, body').stop().animate({
+					scrollTop: $(anchor.attr('href')).offset().top - 50
+				}, 1500);
+				e.preventDefault();
+			});		
+
+	}); 	
+
+				
+})
+
+
+// ** end **//
+
+
+
+// ** pop up ***//
+
